8afff2211fcd14c4bd0617fba9c4cde93c2309e2
Viktor Sandström <Viktor Sandstrm>
2025-07-28 17:22:01 +0200

    shared connection behavior trait

--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -6,7 +6,6 @@ use crate::{
 };
 
 use midir::{
-  ConnectError, 
   MidiInput,
   MidiInputConnection,
   MidiInputPort,
@@ -38,23 +37,6 @@ impl Output {
     self.conn.send(message)
   }
 
-  fn init_client() -> Result<MidiOutput, String> {
-    match MidiOutput::new("cpu") {
-      Ok(output) => Ok(output),
-      Err(e) => Err(format!("could not create MIDI output: {}", e))
-    }
-  }
-
-  fn validate_port(output: &MidiOutput, device: &'static str, ports: Vec<MidiOutputPort>) -> Result<MidiOutputPort, String> {
-    match ports
-      .iter()
-      .find(|p| 
-        Some(device) == output.port_name(p).ok().as_deref()) {
-      Some(p) => Ok(p.clone()),
-      None => Err("could not find output port".to_owned())
-    }
-  }
-
   fn connect(output: MidiOutput, port: &MidiOutputPort, device: &'static str) -> Result<MidiOutputConnection, String> {
     match output.connect(port, device) {
       Ok(conn) => Ok(conn),
@@ -71,6 +53,7 @@ impl Output {
     Self::connect(output, &port, device)
   }
 }
+
 
 /// Convenience struct for creating a Midi Input connection
 /// ```
@@ -119,46 +102,66 @@ impl<T, F> Input<T, F>
     }
   } 
 
-
   #[inline]
   fn init(device: &'static str, data: T, callback: F) -> Result<MidiInputConnection<T>, String> {
     let input = Self::init_client()?;
     let port = Self::validate_port(&input, device, input.ports())?;
     Self::connect(input, &port, device, data, callback)
   }
-  
-  #[inline]
-  fn init_client() -> Result<MidiInput, String> {
-    match MidiInput::new("cpu") {
-      Ok(input) => Ok(input),
-      Err(e) => Err(format!("could not create MIDI input: {}", e))
+}
+
+
+trait MidiConnection {
+  type MidiType;
+  type MidiPort;
+  fn validate_port(input: &Self::MidiType, device: &'static str, ports: Vec<Self::MidiPort>) -> Result<Self::MidiPort, String>;
+  fn init_client() -> Result<Self::MidiType, String>;
+}
+
+
+impl MidiConnection for Output {
+  type MidiType = MidiOutput;
+  type MidiPort = MidiOutputPort;
+  fn validate_port(output: &Self::MidiType, device: &'static str, ports: Vec<Self::MidiPort>) -> Result<Self::MidiPort, String> {
+    match ports
+      .iter()
+      .find(|p| 
+        Some(device) == output.port_name(p).ok().as_deref()) {
+      Some(p) => Ok(p.clone()),
+      None => Err("could not find output port".to_owned())
     }
   }
+  
+  fn init_client() -> Result<Self::MidiType, String> {
+    match Self::MidiType::new("cpu") {
+      Ok(output) => Ok(output),
+      Err(e) => Err(format!("could not create MIDI output: {}", e))
+    }
+  }
+}
 
-  #[inline]
-  fn validate_port(input: &MidiInput, device: &'static str, ports: Vec<MidiInputPort>) -> Result<MidiInputPort, String> {
+impl<T, F> MidiConnection for Input<T, F> 
+  where 
+    T: Send + 'static,
+    F: FnMut(u64, &[u8], &mut T) + Send + 'static,
+{
+  type MidiType = MidiInput;
+  type MidiPort = MidiInputPort;
+  fn validate_port(input: &Self::MidiType, device: &'static str, ports: Vec<Self::MidiPort>) -> Result<Self::MidiPort, String> {
     match ports
       .iter()
       .find(|p| 
         Some(device) == input.port_name(p).ok().as_deref()) {
       Some(p) => Ok(p.clone()),
-      None => Err(format!("could not find input port: {device}").to_owned())
+      None => Err("could not find input port".to_owned())
+    }
+      
+  }
+  
+  fn init_client() -> Result<Self::MidiType, String> {
+    match Self::MidiType::new("cpu") {
+      Ok(input) => Ok(input),
+      Err(e) => Err(format!("could not create MIDI input: {}", e))
     }
   }
 }
-
-pub fn fetch_output_port(name: &str) -> Result<MidiOutputConnection, ConnectError<MidiOutput>> {
-  let output = MidiOutput::new("cpu").unwrap_or_else(|_| panic!("could not create MIDI output"));
-  let ports = output.ports();
-  
-  let port = ports
-    .iter()
-    .find(|p| name == output
-      .port_name(p)
-      .unwrap())
-    .unwrap_or_else(|| panic!("{name} is not available as midi output"));
-  
-  output.connect(port, name)
-
-}
-
